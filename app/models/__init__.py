58725fe17cb94faf77a5ccf34825ab3416143943
Troy S <tscottcounsel@gmail.com>
2026-03-26 10:22:31 -0400

    Remove broken Vendor model export from app.models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,7 @@
-from .application import Application
+﻿from .application import Application
 from .event import Event
 from .review import Review
 from .slot import Slot
-from .vendor import Vendor
 
-__all__ = ["Event", "Application", "Vendor", "Slot", "Review"]
+__all__ = ["Event", "Application", "Slot", "Review"]
+
